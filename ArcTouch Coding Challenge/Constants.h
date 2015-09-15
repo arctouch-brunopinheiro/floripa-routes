409d060214134776d368ee30243b41fa18923cff
Mike Quade <mike.quade@bravi.com.br>
2015-09-14 23:57:59 -0300

    Added prefix headers and constants

---
+++ ArcTouch Coding Challenge/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  ArcTouch Coding Challenge
+//
+//  Created by Mike Quade on 9/14/15.
+//  Copyright (c) 2015 Mike Quade. All rights reserved.
+//
+
+#ifndef ArcTouch_Coding_Challenge_Constants_h
+#define ArcTouch_Coding_Challenge_Constants_h
+
+
+#endif
